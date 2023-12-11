4714973fd62d7296c0b309b11f4b13bdb251edc7
MicheleSerafini <serafini.michele01@gmail.com>
2023-12-11 15:17:23 +0100

    ADD > Skeleton Setup

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,27 @@
+<header>
+
+  <section class="small-banner">
+    <div class="container">
+      <div class="row">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad, modi.
+      </div>
+    </div>
+  </section>
+
+  <nav class="nav-section">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          logo
+        </div>
+        <div class="col">
+          navbar links
+        </div>
+        <div class="col">
+          searchbar
+        </div>
+      </div>
+    </div>
+  </nav>
+
+</header>
