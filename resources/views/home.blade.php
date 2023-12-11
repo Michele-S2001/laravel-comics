a7e8c8f80832d86970ea4753f2823ebd4d602124
MicheleSerafini <serafini.michele01@gmail.com>
2023-12-11 17:52:56 +0100

    FEAT > Home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,59 @@
 @section('page-title', 'Home')
 
 @section('content')
-  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, omnis ea eaque non amet laboriosam autem? Nihil quae ratione cumque aperiam, laboriosam inventore dignissimos voluptatum rem sequi atque repudiandae eius esse doloremque vel, repellat sint quaerat nisi error quasi voluptatibus? Ipsa deleniti esse rerum ea mollitia! Eius odio non consequatur, repudiandae corporis vitae laudantium maiores adipisci aspernatur optio sequi pariatur minus consequuntur corrupti quam dignissimos nobis saepe doloremque porro quo perferendis incidunt. Quibusdam velit doloribus iusto! Placeat, modi dicta. Quia vitae non accusamus dolor impedit laborum, ea, vel, fuga porro eum dolorem provident veniam est soluta. Velit quos adipisci reprehenderit perferendis vero unde quasi molestiae repellendus doloremque nostrum! Similique unde id sint iste dolorum harum consectetur temporibus nemo asperiores soluta distinctio voluptas incidunt tenetur libero, dicta culpa illum? Magni error, perspiciatis consectetur, aliquid accusantium laborum cupiditate, eius qui animi non adipisci voluptatem? Voluptatem dolorem pariatur aperiam perferendis nemo minus, nihil quam nobis qui ipsa asperiores eveniet consequuntur est aut, fugit veniam maxime laudantium inventore. Numquam, deserunt assumenda minima voluptate veniam, adipisci blanditiis maiores minus reprehenderit, culpa sed aspernatur incidunt! Vero delectus cupiditate tenetur magni. In autem, beatae neque, mollitia sapiente tempora et iste provident laborum velit nemo fuga eum quia.</p>
+  <section class="current-series text-light">
+    <div class="container">
+      <h3 class="text-uppercase d-inline-block">current series</h3>
+      <div class="row">
+        @foreach ($comics as $item)
+            
+          <div class="col-2 mb-4">
+            <img class="w-100 mb-2" src="{{ $item['thumb'] }}">
+            <span class="text-uppercase comic-title">{{ $item['title'] }}</span>
+          </div>
+
+        @endforeach
+      </div>
+    </div>
+    <div class="load-more text-center text-uppercase py-4">
+      <a class="load-btn" href="#">load more</a>
+    </div>
+  </section>
+
+  <section class="dc-more text-light py-5">
+    <div class="container">
+      <div class="row justify-content-between align-items-center">
+        <div class="col-auto">
+          <div class="dc-more__card">
+            <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}">
+            <span class="text-uppercase">digital comics</span>
+          </div>
+        </div>
+        <div class="col-auto">
+          <div class="dc-more__card">
+            <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}">
+            <span class="text-uppercase">digital comics</span>
+          </div>
+        </div>
+        <div class="col-auto">
+          <div class="dc-more__card">
+            <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}">
+            <span class="text-uppercase">digital comics</span>
+          </div>
+        </div>
+        <div class="col-auto">
+          <div class="dc-more__card">
+            <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}">
+            <span class="text-uppercase">digital comics</span>
+          </div>
+        </div>
+        <div class="col-auto">
+          <div class="dc-more__card">
+            <img class="power-svg" src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}">
+            <span class="text-uppercase">digital comics</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 @endsection
